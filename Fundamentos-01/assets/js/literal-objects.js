60fdc4af9eb028f519de47143756f4d3c96ca2d3
Felipe Baracaldo <felipe.baracaldo1118@icloud.com>
2026-07-25 23:14:17 -0500

    objects and methos

--- Fundamentos-01/assets/js/literal-objects.js
+++ Fundamentos-01/assets/js/literal-objects.js
@@ -1,5 +1,8 @@
 //using brackets is a way to indentify thats an Object in jS
 //inside the object there are to things to take into account the key and it's value, the Key is the one that gives a reference for the element.
+//creating an object using cost means that the properties inside the object can be mutable such as using let. BUT we cannot be able to change it's purpose like changing the objecto to a string.
+
+
 let personaje = {
     nombre: 'Tony Stark',
     edad: 25,
@@ -15,6 +18,8 @@ let personaje = {
     ]
 }
 
+
+
 console.log(personaje)
 //to look for info inside the objects we can do it on different ways. 1. Using dot (.) notation, 2. using []. 
 console.log(personaje['trajes'])
@@ -26,3 +31,30 @@ personaje.trajes.push('SpiderMan')
 console.log(personaje.trajes.length, personaje.trajes)
 //to search for the latest element inside the array
 console.log(personaje.trajes[personaje.trajes.length - 1])
+
+
+//HOW TO DELETE PROPERTIES INSIDE THE OBJECTS
+delete personaje.edad
+console.log(personaje)
+
+// IF WE NEED TO SEARCH INSIDE OF EACH INFO AND OBTAIN THEIR VALUES WE CAN USE :
+const entriesPares = Object.entries(personaje)
+//this will give a value for each propertie and value such as an array so it's easier to search inside the objects
+console.log(entriesPares)
+
+// Create a new propertie inside the object. 
+personaje.casado = true;
+console.log(personaje)
+
+//to block the modification for the properties we sould use:
+//this will freeze the object at the moment when the function/method is called. This will only affect the properties that have their assigned value, but no to the objects or arrays inside the main object.
+Object.freeze(personaje)
+personaje.dinero = 10000000;
+console.log(personaje)
+
+//print the properties name created inside the object.
+let properties = Object.getOwnPropertyNames (personaje)
+//print the values
+let values = Object.values(personaje)
+console.log(properties)
+console.log(values)
