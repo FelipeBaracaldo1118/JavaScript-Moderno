7d00bc2ffd63422d45e296de4015cd74d0e81fda
Felipe Baracaldo <felipe.baracaldo1118@icloud.com>
2026-07-27 21:40:36 -0500

    return and new arrow functions

--- Fundamentos-01/assets/js/functions.js
+++ Fundamentos-01/assets/js/functions.js
@@ -1,6 +1,7 @@
 //FUNCTIONS -> The main idea of functions is to store the logic for a process we can call/run different times 
 function saludar(){
     console.log('Hello World')
+    return 1;
 }
 //it's a good practice to define first the function and then call it. 
 
@@ -14,9 +15,11 @@ const saludar2 = function(){
 
 function prueba(nombre){
     console.log('Hola '+ nombre)
+    return 10;
 }
-prueba('felipe')
 
+let pruebaretorno= prueba('felipe')
+console.log(pruebaretorno)
 // If theres any other arguments that are not included/declared previously on the traditional functions store those arguments in a special space called arguments.
 function creandoarguemntos(){
     console.log(arguments)
@@ -27,5 +30,22 @@ creandoarguemntos(1, 'felipe', 'hablando')
 //Arrow Functions -> Lambda functions 
 
 let arrow = () => {
-    
+
 }
+
+//each previous functions always returns an value
+//each function that is not using the reserve word return it's value should be undefined
+//coding lines typed after the return will not be executed. Basically return, quits from the function 
+
+sumar = (a,b) =>{
+    return a + b;
+}
+console.log(sumar(1,2))
+
+//this is a new way to make arrow functions to return when it's the only line of code.
+
+let sumar2 = (a,b) => a+b;
+console.log(sumar2 (22,1))
+
+let getAleatorio = () => Math.random();
+console.log(getAleatorio())
